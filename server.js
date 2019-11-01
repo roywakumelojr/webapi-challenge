29ad12302014b24f0f96dbe74522b94bd8c44b40
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-11-01 11:52:55 -0400

    Project Router Set Up Complete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const server = express();
 const helmet = require('helmet');
+const projectRouter = require('./projectRouter');
 
 function logger(req, res, next) {
     console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
@@ -12,5 +13,6 @@ function logger(req, res, next) {
 server.use(helmet());
 server.use(logger);
 server.use(express.json());
+server.use(projectRouter);
 
 module.exports = server;
